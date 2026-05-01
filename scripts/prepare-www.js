9b466c998713a49ebffd9b0e286754a9225d94ee
Romar <romarmarquez777@gmail.com>
2026-05-01 18:51:47 -0300

    Corregido logo y diseño del login para móviles

--- scripts/prepare-www.js
+++ scripts/prepare-www.js
@@ -21,4 +21,27 @@ for (const name of fs.readdirSync(root)) {
   fs.copyFileSync(full, path.join(www, name));
 }
 
+function copyTree(srcDir, destDir) {
+  if (!fs.existsSync(srcDir)) return;
+  fs.mkdirSync(destDir, { recursive: true });
+  for (const ent of fs.readdirSync(srcDir, { withFileTypes: true })) {
+    const src = path.join(srcDir, ent.name);
+    const dest = path.join(destDir, ent.name);
+    if (ent.isDirectory()) {
+      copyTree(src, dest);
+    } else if (ALLOW.test(ent.name)) {
+      fs.copyFileSync(src, dest);
+    }
+  }
+}
+
+copyTree(path.join(root, "assets"), path.join(www, "assets"));
+
+const legacyLogo = path.join(root, "almacenhoney.png");
+const legacyLogoDest = path.join(www, "assets", "almacenhoney.png");
+if (fs.existsSync(legacyLogo)) {
+  fs.mkdirSync(path.dirname(legacyLogoDest), { recursive: true });
+  fs.copyFileSync(legacyLogo, legacyLogoDest);
+}
+
 console.log("prepare-www: archivos copiados a www/");
